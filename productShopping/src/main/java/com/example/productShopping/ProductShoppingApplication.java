494d04c755dd0585951c5ebcccd47c46f7176a21
new  name <kenan.gakhramanov@mail.ru>
2024-12-21 15:33:59 +0300

    start

---
+++ productShopping/src/main/java/com/example/productShopping/ProductShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.productShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductShoppingApplication.class, args);
+	}
+
+}
---
+++ productShopping/src/test/java/com/example/productShopping/ProductShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
